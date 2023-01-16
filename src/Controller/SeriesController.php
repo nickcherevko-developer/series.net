29bfe281a2a0a20f00844c98f3e87537d7e261b7
nick <nickcherevko.developer@gmail.com>
2023-01-16 09:37:51 +0200

    v1.0.1-create controller series and show

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -1,18 +1,32 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Series;
+use App\Repository\SeriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class SeriesController extends AbstractController
 {
-    /**
-     * @Route("/")
-     */
-    public function index()
+
+    #[Route('/', name: 'homepage')]
+    public function index(Environment $twig, SeriesRepository $seriesRepository): Response
     {
-        return $this->render('series/index.html.twig', [
-            'title' => 'Hello, Word!',
-        ]);
+        return new Response($twig->render('series/index.html.twig', [
+            'seriess' => $seriesRepository->findAll(),
+        ]));
+
+
     }
+
+    #[Route('/series/{id}', name: 'series')]
+    public function show(Environment $twig, Series $series): Response
+    {
+        return new Response($twig->render('series/show.html.twig', [
+            'series' => $series
+        ]));
+    }
+
 }
